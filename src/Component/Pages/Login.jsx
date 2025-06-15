b59f9328704a8614ba52571219a164f2318d60b5
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-06-16 00:16:30 +0600

    sign in with google popup setup done

--- src/Component/Pages/Login.jsx
+++ src/Component/Pages/Login.jsx
@@ -1,84 +1,144 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router';
-import { auth } from '../../Firebase/Firebase.init';
-import { toast, ToastContainer } from 'react-toastify';
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { auth } from "../../Firebase/Firebase.init";
+import { toast, ToastContainer } from "react-toastify";
+
+const provider = new GoogleAuthProvider();
 
 const Login = () => {
-     const [errorMessage, setErrorMessage] = useState('')
-    const navigate = useNavigate()
-    const handleSignIn = e => {
-        e.preventDefault()
-        const form = e.target
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        const UserInfo = {email, password}
-        console.log(UserInfo)
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const UserInfo = { email, password };
+    console.log(UserInfo);
 
+    // reset
+    setErrorMessage("");
 
-        // reset 
-        setErrorMessage('')
+    // login user
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        toast.success(" Account created successfully!");
+        form.reset();
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(`${error.message}`);
+      });
+  };
 
+  // Signin with google popup
 
-        // login user 
-        signInWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result)
-            toast.success(" Account created successfully!");
-                    form.reset();
-                    navigate('/')
-        }).catch(error => {
-            console.log(error)
-            toast.error(`${error.message}`);
-        })
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(" Google Signin User:", user);
+        toast.success("Signed in Successfully");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error("Google sign in failed: " + error.message);
+      });
+  };
 
-    }
+  return (
+    <div className="w-full max-w-md mx-auto my-20 p-4 rounded-md border border-[#73B21A] shadow-xl sm:p-8 dark:bg-gray-50 dark:text-gray-800">
+      <h2 className="mb-3 text-3xl font-semibold text-center">
+        Login to your account
+      </h2>
+      <p className="text-sm text-center dark:text-gray-600">
+        Dont have account?{" "}
+        <Link to="/signup" className="text-blue-500 underline">
+          Sign Up here
+        </Link>
+      </p>
+      <div className="my-6 space-y-4">
+        <button
+          onClick={handleGoogleSignIn}
+          aria-label="Login with Google"
+          type="button"
+          className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            className="w-5 h-5 fill-current"
+          >
+            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+          </svg>
+          <p>Login with Google</p>
+        </button>
+      </div>
+      <div className="flex items-center w-full my-4">
+        <hr className="w-full dark:text-gray-600" />
+        <p className="px-3 dark:text-gray-600">OR</p>
+        <hr className="w-full dark:text-gray-600" />
+      </div>
+      <form
+        onSubmit={handleSignIn}
+        noValidate=""
+        action=""
+        className="space-y-8"
+      >
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <label htmlFor="email" className="block text-sm">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="leroy@jenkins.com"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
+            />
+          </div>
+          <div className="space-y-2">
+            <div className="flex justify-between">
+              <label htmlFor="password" className="text-sm">
+                Password
+              </label>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                className="text-xs hover:underline dark:text-gray-600"
+              >
+                Forgot password?
+              </a>
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="*****"
+              className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-[#73B21A] hover:bg-[#008236] hover:text-white w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
+        >
+          Sign in
+        </button>
+      </form>
 
-
-
-    return (
-        <div className="w-full max-w-md mx-auto my-20 p-4 rounded-md border border-[#73B21A] shadow-xl sm:p-8 dark:bg-gray-50 dark:text-gray-800">
-	<h2 className="mb-3 text-3xl font-semibold text-center">Login to your account</h2>
-	<p className="text-sm text-center dark:text-gray-600">Dont have account? <Link to='/signup' className='text-blue-500 underline'>Sign Up here</Link>
-
-	</p>
-	<div className="my-6 space-y-4">
-		<button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-				<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-			</svg>
-			<p>Login with Google</p>
-		</button>
-	
-	</div>
-	<div className="flex items-center w-full my-4">
-		<hr  className="w-full dark:text-gray-600" />
-		<p className="px-3 dark:text-gray-600">OR</p>
-		<hr  className="w-full dark:text-gray-600" />
-	</div>
-	<form onSubmit={handleSignIn} noValidate="" action="" className="space-y-8">
-		<div className="space-y-4">
-			<div className="space-y-2">
-				<label htmlFor="email" className="block text-sm">Email address</label>
-				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600" />
-			</div>
-			<div className="space-y-2">
-				<div className="flex justify-between">
-					<label htmlFor="password" className="text-sm">Password</label>
-					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">Forgot password?</a>
-				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600" />
-			</div>
-		</div>
-		<button type="submit" className="bg-[#73B21A] hover:bg-[#008236] hover:text-white w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign in</button>
-	</form>
-
-        {
-            errorMessage && <p className='text-red-500'>{errorMessage}</p>
-        }
- <ToastContainer position="top-center" />
-</div>
-    );
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      <ToastContainer position="top-center" />
+    </div>
+  );
 };
 
 export default Login;
--- src/Component/Pages/SignUp.jsx
+++ src/Component/Pages/SignUp.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
 import { auth } from "../../Firebase/Firebase.init";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { Link, useNavigate } from "react-router";
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 const SignUp = () => {
   const [errorMessage, setErrorMessage] = useState("");
 
-	const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSignUp = (e) => {
     e.preventDefault();
@@ -37,7 +42,9 @@ const SignUp = () => {
     // Password strength check
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     if (!passwordRegex.test(password)) {
-      toast.error("Password must be at least 8 characters and include uppercase, lowercase, and a number.");
+      toast.error(
+        "Password must be at least 8 characters and include uppercase, lowercase, and a number."
+      );
       return;
     }
 
@@ -49,22 +56,22 @@ const SignUp = () => {
       .then((result) => {
         console.log(result);
 
-		// update user profile 
-		const profile = {
-			displayName: fullname,
-			photoURL: photo
-		}
-		updateProfile(auth.currentUser, profile)
-		.then (() => {
-			console.log ('User profile updated')
-		}).catch (error =>{
-			console.log(error)
-		})
-
+        // update user profile
+        const profile = {
+          displayName: fullname,
+          photoURL: photo,
+        };
+        updateProfile(auth.currentUser, profile)
+          .then(() => {
+            console.log("User profile updated");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
         toast.success(" Account created successfully!");
-        navigate('/login')
-		form.reset();
+        navigate("/login");
+        form.reset();
       })
       .catch((error) => {
         console.error(error);
@@ -73,24 +80,20 @@ const SignUp = () => {
       });
   };
 
+  // Signup with google popup
 
-  // Signup with google popup 
-
- const handleGoogleSignup = () => {
-  signInWithPopup(auth, provider)
-  .then ((result) => {
-    const user = result.user;
-    console.log(user)
-    toast.success("Signed in Successfully")
-    navigate('/')
-  }).catch((error) => {
-    toast.error(' Google signup failed: ' + error.message)
-  })
- }
-
-
-
-
+  const handleGoogleSignup = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("Signed in Successfully");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(" Google signup failed: " + error.message);
+      });
+  };
 
   return (
     <div className="w-full max-w-md mx-auto my-20 p-4 rounded-md border border-[#73B21A] shadow-xl sm:p-8 dark:bg-gray-50 dark:text-gray-800">
@@ -98,48 +101,111 @@ const SignUp = () => {
         Register An Account
       </h2>
       <p className="text-sm text-center dark:text-gray-600">
-        Already have an account? <Link to='/login' className="text-blue-500 underline"> Sign in here </Link>
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-500 underline">
+          {" "}
+          Sign in here{" "}
+        </Link>
       </p>
 
-    	<div className="my-6 space-y-4">
-		<button onClick={handleGoogleSignup} aria-label="Login with Google" type="button" className="cursor-pointer flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-				<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-			</svg>
-			<p>Login with Google</p>
-		</button>
-	
-	</div>
+      <div className="my-6 space-y-4">
+        <button
+          onClick={handleGoogleSignup}
+          aria-label="Login with Google"
+          type="button"
+          className="cursor-pointer flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            className="w-5 h-5 fill-current"
+          >
+            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+          </svg>
+          <p>Login with Google</p>
+        </button>
+      </div>
 
       <form onSubmit={handleSignUp} className="space-y-8 mt-6">
         <div className="space-y-4">
           <div>
-            <label htmlFor="name" className="block text-sm">Full Name</label>
-            <input type="text" name="fullname" id="name" placeholder="John Andre" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="name" className="block text-sm">
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="fullname"
+              id="name"
+              placeholder="John Andre"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
           <div>
-            <label htmlFor="phone" className="block text-sm">Phone</label>
-            <input type="number" name="phone" id="phone" placeholder="0123456789" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="phone" className="block text-sm">
+              Phone
+            </label>
+            <input
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="0123456789"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
           <div>
-            <label htmlFor="photo" className="block text-sm">Photo URL</label>
-            <input type="text" name="photo" id="photo" placeholder="http://photourl.com" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="photo" className="block text-sm">
+              Photo URL
+            </label>
+            <input
+              type="text"
+              name="photo"
+              id="photo"
+              placeholder="http://photourl.com"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
           <div>
-            <label htmlFor="email" className="block text-sm">Email address</label>
-            <input type="email" name="email" id="email" placeholder="example@mail.com" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="email" className="block text-sm">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@mail.com"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
           <div>
-            <label htmlFor="password" className="text-sm">Password</label>
-            <input type="password" name="password" id="password" placeholder="********" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="password" className="text-sm">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="********"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
           <div>
-            <label htmlFor="repassword" className="text-sm">Re-Password</label>
-            <input type="password" name="repassword" id="repassword" placeholder="********" className="w-full px-3 py-2 border rounded-md" />
+            <label htmlFor="repassword" className="text-sm">
+              Re-Password
+            </label>
+            <input
+              type="password"
+              name="repassword"
+              id="repassword"
+              placeholder="********"
+              className="w-full px-3 py-2 border rounded-md"
+            />
           </div>
         </div>
 
-        <button type="submit" className="bg-[#73B21A] hover:bg-[#008236] hover:text-white w-full px-8 py-3 font-semibold rounded-md">
+        <button
+          type="submit"
+          className="bg-[#73B21A] hover:bg-[#008236] hover:text-white w-full px-8 py-3 font-semibold rounded-md"
+        >
           Sign up
         </button>
       </form>
