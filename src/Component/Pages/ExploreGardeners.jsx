530bd74ad2be858119a8af096186956800f7e2e1
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-06-17 00:02:21 +0600

    gardener profile inserted

--- src/Component/Pages/ExploreGardeners.jsx
+++ src/Component/Pages/ExploreGardeners.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ExploreGardenersCard from './ExploreGardenersCard';
 
 const ExploreGardeners = () => {
+
+    const [profiles, setProfiles] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/profile')
+        .then(res => res.json())
+        .then(data => setProfiles(data))
+    }, []);
+
     return (
-        <div>
-            This is Explore Gardeners
+        <div className='grid grid-cols-1 md:grid-cols-3 max-w-11/12 mx-auto gap-6 my-20'>
+           {
+            profiles.map(profile => <ExploreGardenersCard profile={profile}></ExploreGardenersCard>)
+           }
         </div>
     );
 };
