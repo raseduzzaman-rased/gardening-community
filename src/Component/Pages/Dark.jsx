a6a724eb4a56ed3a4ad738c8c737f85dca22d645
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-06-19 11:12:03 +0600

    enable the dark mode

--- src/Component/Pages/Dark.jsx
+++ src/Component/Pages/Dark.jsx
@@ -5,7 +5,7 @@ const Dark = () => {
         <div>
             <label className="swap swap-rotate">
   {/* this hidden checkbox controls the state */}
-  <input type="checkbox" className="theme-controller" value="synthwave" />
+  <input type="checkbox" className="theme-controller" value="dark" />
 
   {/* sun icon */}
   <svg
